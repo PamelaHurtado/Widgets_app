1b1b8461efb3c3511f8a7bbc0cc7e61467d93fb0
Pamela <phurtado@infoblock.mx>
2025-09-04 10:20:00 -0600

    avanceThemeyHome

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.purple,
+  Colors.blue,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.red,
+];
+
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }): assert(selectedColor >= 0,'selectedColor must be greater than 0'),
+      assert(selectedColor < colorList.length,'selectedColor must be smaller than ${colorList.length-1}');
+
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[ selectedColor ],
+  );
+
+}
